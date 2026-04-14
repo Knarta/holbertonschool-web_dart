58927a6f43342791c482daa2791dd5eb68ecc7e9
knarta <charquen10@gmail.com>
2026-04-14 14:07:34 +0200

    Add addition and subtraction functions with a display method in Dart program

---
+++ 0x01-dart-function/2-sumfunc.dart
@@ -0,0 +1,11 @@
+int add(int a, int b) {
+    return a + b;
+}
+
+int sub(int a, int b) {
+    return a - b;
+}
+
+String showFunc(int a, int b) {
+    return 'Add $a + $b = ${a + b}\nSub $a - $b = ${a - b}';
+}
