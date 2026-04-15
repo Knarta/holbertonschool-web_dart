f1c30c897d9b00e6210bfad2554a7c304d00bec4
knarta <charquen10@gmail.com>
2026-04-15 16:32:22 +0200

    Update import statement in getUserId function and change userData variable type for consistency

--- 0x03-asynchronous/1-get_user_id.dart
+++ 0x03-asynchronous/1-get_user_id.dart
@@ -1,8 +1,8 @@
-import '0-util.dart';
+import '1-util.dart';
 import 'dart:convert';
 
 Future<String> getUserId() async {
-  String userData = await fetchUserData();
+  var userData = await fetchUserData();
   Map<String, dynamic> userMap = jsonDecode(userData);
   return userMap['id'];
 }
