a872c5a89d54c2ca159b8c6b3b2dbc6a9076759b
knarta <charquen10@gmail.com>
2026-04-15 16:29:52 +0200

    Refactor getUserId function to decode user data and return user ID from JSON

--- 0x03-asynchronous/1-get_user_id.dart
+++ 0x03-asynchronous/1-get_user_id.dart
@@ -1,11 +1,8 @@
 import '0-util.dart';
+import 'dart:convert';
 
 Future<String> getUserId() async {
   String userData = await fetchUserData();
-  return userData;
-}
-
-Future<void> main() async {
-  String userId = await getUserId();
-  print("User ID: $userId");
+  Map<String, dynamic> userMap = jsonDecode(userData);
+  return userMap['id'];
 }
